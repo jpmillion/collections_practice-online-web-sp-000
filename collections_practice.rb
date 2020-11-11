57271502eea4b54d2e2aab5df9c0e91bff51bf02
John Million <jpmillion@comcast.net>
2020-11-11 02:49:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(list)
+  return list.sort
+end
 
+def sort_array_desc(list)
+  return list.sort.reverse
+end
+  
+def sort_array_char_count(list)
+  return list.sort { |s1, s2| s1.length <=> s2.length } 
+end
+
+def swap_elements(list)
+  temp = list[1]
+  list[1] = list[2]
+  list[2] = temp 
+  return list 
+end
+
+def reverse_array(array)
+  return array.reverse 
+end
+
+def kesha_maker(array)
+  modified_array = []
+  array.each do |s| 
+    s[2] = "$"
+    modified_array.push(s)
+  end
+  return modified_array
+end
+
+def find_a(array)
+  return array.scan(/\Aa/i).flatten
+end
+
+  
